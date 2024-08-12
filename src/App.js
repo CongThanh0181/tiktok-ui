716657ad29f56731e72af2667c54725bc1dfe413
thanh cong luu <luuthanhcong1810@gmail.com>
2024-08-12 21:33:57 +0700

    config router for web  and build default layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layouts';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((router, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (router.layout) {
+                            Layout = router.layout;
+                        } else if (router.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = router.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={router.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
