c7231e0920517e454eae7538ec50051bf7c32b43
thanh cong luu <luuthanhcong1810@gmail.com>
2024-08-13 22:16:47 +0700

    install library classNames, build scss DefaultLayout, fix GlobalStyles

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,17 @@
+// Library css
+import classNames from 'classnames/bind';
+
+import styles from './Header.module.scss';
+
+//Use library to make classnames css
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <h1 className={cx('wrapper')}>
+            <div className={cx('inner')}>{/* contents */}</div>
+        </h1>
+    );
 }
 
 export default Header;
